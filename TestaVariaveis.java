715baa87466427207d3702962d1fc63cc8b632a2
ellen <99822361+wwwellen@users.noreply.github.com>
2023-06-14 23:19:00 -0300

    add the files

---
+++ TestaVariaveis.java
@@ -0,0 +1,9 @@
+public class TestaVariaveis{
+
+    public static void main(String [] args){
+    	double salario = 1250.70;
+    	System.out.println(salario);
+    	
+    	System.out.println("meu salário é " + salario);
+    }
+}
